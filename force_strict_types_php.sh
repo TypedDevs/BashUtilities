afa97b610cc9bff44036a76411da0908b743d100
khru <evrtrabajo@gmail.com>
2019-07-24 10:24:51 +0200

    Exclude vendor folder

--- force_strict_types_php.sh
+++ force_strict_types_php.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "The affected files will be:"
-grep -R --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":" 
+grep -R --exclude-dir="vendor" --include="*.php" "<?php" . |  awk '{print $1 }' FS=":" 
  
 
 # Change to strict types
-grep -R --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":"  | xargs sed -i 's/declare(strict_types=1);//g'
-grep -R --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":"  | xargs sed -i 's/declare(strict_types = 1);//g' 
-grep -R --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":"   | xargs sed -i 's/<?php/<?php\n\ndeclare(strict_types=1);/g'
+grep -R --exclude-dir="vendor" --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":"  | xargs sed -i 's/declare(strict_types=1);//g'
+grep -R --exclude-dir="vendor" --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":"  | xargs sed -i 's/declare(strict_types = 1);//g' 
+grep -R --exclude-dir="vendor" --include="*.php"  "<?php" . |  awk '{print $1 }' FS=":"   | xargs sed -i 's/<?php/<?php\n\ndeclare(strict_types=1);/g'
 echo "Done"
